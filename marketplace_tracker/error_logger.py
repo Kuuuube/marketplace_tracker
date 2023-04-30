37990bbfaf98daa082be2b1003b4b31a147171bf
kuuuube <hexagonisalie@gmail.com>
2023-04-30 16:57:57 -0400

    Use UTF8 for error logger

--- marketplace_tracker/error_logger.py
+++ marketplace_tracker/error_logger.py
@@ -3,7 +3,7 @@ from datetime import datetime,timezone
 def error_log(message,error):
     try:
         utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
-        with open ("error_log.txt", "a") as log_file:
+        with open ("error_log.txt", "a", encoding="utf8") as log_file:
             log_file.write("\n")
             log_file.write(utc_time + ", ")
             log_file.write(message + ", ")
