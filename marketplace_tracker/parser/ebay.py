c5c54d338bc66bc22d93e050d72342cb65273f1b
kuuuube <hexagonisalie@gmail.com>
2023-04-30 10:37:22 -0400

    Unescape html character entity references

--- marketplace_tracker/parser/ebay.py
+++ marketplace_tracker/parser/ebay.py
@@ -2,6 +2,7 @@ import re
 import requests
 import time
 import error_logger
+import html
 
 def page_parser(request_delay):
     url_request_list = []
@@ -39,7 +40,7 @@ def page_parser(request_delay):
                 item_info["thumbnail"] = ""
 
             if len(title) > 0:
-                item_info["title"] = re.sub("&#34;", "\"", strip_excess_html(title[0]))
+                item_info["title"] = strip_excess_html(title[0])
             else:
                 item_info["title"] = ""
 
@@ -78,4 +79,4 @@ def page_parser(request_delay):
     return item_info_list
 
 def strip_excess_html(string):
-    return re.sub("<.*?>", "", string)
+    return html.unescape(re.sub("<.*?>", "", string))
