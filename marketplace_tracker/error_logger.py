406846381a20193781e6009ba91052ae27fd4317
kuuuube <hexagonisalie@gmail.com>
2023-05-03 13:51:37 -0400

    error_logger use single write and add newline as suffix instead of prefix

--- marketplace_tracker/error_logger.py
+++ marketplace_tracker/error_logger.py
@@ -4,10 +4,7 @@ def error_log(message,error):
     try:
         utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
         with open ("error_log.txt", "a", encoding="utf8") as log_file:
-            log_file.write("\n")
-            log_file.write(utc_time + ", ")
-            log_file.write(message + ", ")
-            log_file.write(str(error))
+            log_file.write(utc_time + ", " + message + ", " + str(error) + "\n")
         print(message)
         print(error)
     except Exception as e:
