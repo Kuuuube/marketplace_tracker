98f328f93a3fd4323740fc7ce30eb6c5ecd0eb02
kuuuube <hexagonisalie@gmail.com>
2023-05-02 21:06:57 -0400

    Return empty list when requesting bad section

--- marketplace_tracker/config_handler.py
+++ marketplace_tracker/config_handler.py
@@ -4,7 +4,9 @@ def read(config_file, section, name = None):
     config = configparser.ConfigParser(allow_no_value=True)
     config.read(config_file)
 
-    if section not in config or (name != None and name not in config[section]):
+    if section not in config and name == None:
+        return []
+    elif section not in config or (name != None and name not in config[section]):
         return ""
 
     if name == None:
