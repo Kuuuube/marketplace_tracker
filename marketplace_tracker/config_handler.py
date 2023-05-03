7cf652599f6c4d4d7312647158f6a029a6bc7185
kuuuube <hexagonisalie@gmail.com>
2023-05-02 21:01:14 -0400

    Clean up config_handler and allow reading section keys to list

--- marketplace_tracker/config_handler.py
+++ marketplace_tracker/config_handler.py
@@ -1,10 +1,13 @@
-from configparser import ConfigParser
+import configparser
 
-def config_info(config_file):
-    config = ConfigParser()
+def read(config_file, section, name = None):
+    config = configparser.ConfigParser(allow_no_value=True)
     config.read(config_file)
-    return config
 
-def read(config_file,section,name):
-    config = config_info(config_file)
-    return config.get(section,name)
+    if section not in config or (name != None and name not in config[section]):
+        return ""
+
+    if name == None:
+        return list(config[section])
+
+    return config[section][name]
