c3abeb2cc5e9815125962f73ee2806472594cae0
kuuuube <hexagonisalie@gmail.com>
2023-05-11 09:44:32 -0400

    Error logger convert to string before trying to replace

--- marketplace_tracker/error_logger.py
+++ marketplace_tracker/error_logger.py
@@ -4,7 +4,7 @@ def error_log(message,error):
     try:
         utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
         with open ("error_log.txt", "a", encoding="utf8") as log_file:
-            log_file.write(utc_time + ", " + str(message.replace("\r", r"\r").replace("\n", r"\n")) + ", " + str(error.replace("\r", r"\r").replace("\n", r"\n")) + "\n")
+            log_file.write(utc_time + ", " + str(message).replace("\r", r"\r").replace("\n", r"\n") + ", " + str(error).replace("\r", r"\r").replace("\n", r"\n") + "\n")
         print(message)
         print(error)
     except Exception as e:
