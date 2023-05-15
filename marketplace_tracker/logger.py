6e39e23ed8efabb412e81cb6f0e899342fc1f1ae
kuuuube <hexagonisalie@gmail.com>
2023-05-15 12:09:21 -0400

    Fix logger not writing newlines

--- marketplace_tracker/logger.py
+++ marketplace_tracker/logger.py
@@ -15,7 +15,7 @@ def log(message):
     try:
         utc_time = datetime.now(timezone.utc).strftime("%Y-%m-%d_%H-%M-%S")
         with open ("log.txt", "a", encoding="utf8") as log_file:
-            log_file.write(utc_time + ", " + str(message).replace("\r", r"\r").replace("\n", r"\n"))
+            log_file.write(utc_time + ", " + str(message).replace("\r", r"\r").replace("\n", r"\n") + "\n")
         print(message)
     except Exception as e:
         print("Could not write to log:")
