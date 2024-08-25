6a6c34396a4bddda4dc6e7c3306916c4efd784b0
kuuuube <hexagonisalie@gmail.com>
2024-08-24 21:07:45 -0400

    Fix tradera title parsing

--- marketplace_tracker/parser/tradera.py
+++ marketplace_tracker/parser/tradera.py
@@ -25,7 +25,7 @@ def page_parser(request_delay, request_timeout):
             item_info = {}
             url = re.findall("(?<=href=\")/item/.*?(?=\">)", listing_container)
             thumbnail = re.findall("(?<=<img loading=\"lazy\" src=\").*?(?=\")", listing_container)
-            title = [*re.findall("(?<=<a title=\").*?(?=\")", listing_container), *re.findall("(?<=<a title=\').*?(?=\')", listing_container)]
+            title = [*re.findall("(?<=<a data-link-type=\"next-link\" title=\").*?(?=\")", listing_container), *re.findall("(?<=<a title=\').*?(?=\')", listing_container)]
             price = re.findall("(?<=<span class=\"text-nowrap font-weight-bold font-hansen pr-1\" data-testid=\"price\">).*?(?=</span>)", listing_container)
             buy_it_now_price = re.findall("(?<=<span class=\"text-nowrap text-inter-light pr-1\" data-testid=\"bin-price\">).*?(?=</span>)", listing_container)
             buy_it_now_label = re.findall("(?<=<span data-testid=\"fixedPriceLabel\" class=\"text-nowrap pr-1\">).*?(?=</span>)", listing_container)
